6e9820ba2fef55cb2e70fa88884f7ec42d833e92
MattSegal <mattdsegal@gmail.com>
2016-06-30 21:45:23 +1000

    added jade

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
+from flask import Flask, Blueprint, render_template
 
-from flask import Flask , render_template
+chris_profile = Blueprint('chris_profile', __name__,
+            template_folder='templates', 
+            static_folder='static', static_url_path = '/chris_profile/static')
 
-app = Flask(__name__)
-
-@app.route('/')
+@chris_profile.route('/chris/')
 def homepage():
-    return render_template('./home.html')
+    return render_template('./chris.html')
 
 if __name__ == "__main__":
+    app = Flask(__name__)
+    app.register_blueprint(chris_profile)
     app.run(host= '0.0.0.0',debug=True)
