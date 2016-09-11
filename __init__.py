80a6a867ce3ac355eb5d659b110a8ac798d07821
MattSegal <mattdsegal@gmail.com>
2016-09-11 18:50:08 +1000

    tweak init

--- __init__.py
+++ __init__.py
@@ -6,11 +6,13 @@ chris_profile = Blueprint('chris_profile', __name__,
             static_folder='static', static_url_path = '/chris_profile/static')
 
 app = Flask(__name__)
-app.register_blueprint(chris_profile)
+
 
 @chris_profile.route('/chris/')
 def homepage():
     return render_template('./chris.html')
+
+app.register_blueprint(chris_profile)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=False)
