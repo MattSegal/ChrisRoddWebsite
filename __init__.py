6a2deffd4baec9bb1ae991920a968c812c5b6ea7
MattSegal <mattdsegal@gmail.com>
2016-09-11 18:54:39 +1000

    change from blueprint to stand-alone app

--- __init__.py
+++ __init__.py
@@ -1,18 +1,10 @@
-from flask import Flask, Blueprint, render_template
-
-# clean this up later
-chris_profile = Blueprint('chris_profile', __name__,
-            template_folder='templates', 
-            static_folder='static', static_url_path = '/chris_profile/static')
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@chris_profile.route('/chris/')
+@app.route('/chris/')
 def homepage():
     return render_template('./chris.html')
-
-app.register_blueprint(chris_profile)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=False)
